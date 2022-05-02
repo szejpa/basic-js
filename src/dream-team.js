8348cfb94c9dcdd670efa362d3841cfee3012dfb
szejpa <szejpa@gmail.com>
2022-05-02 17:29:13 +0200

    feat: complete dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let team = [];
+  if (!Array.isArray(members)) return false;
+
+  for (let i = 0; i <= members.length; i++) {
+    if (typeof members[i] === 'string') {
+      team.push(members[i].toUpperCase().trim()[0]);
+    }
+  }
+  return team.sort().join('');
 }
 
 module.exports = {
